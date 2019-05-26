0f9ce8fda2ebee255c6de79f1a66073595276915
faffnir68 <faffnir68@gmail.com>
2019-05-26 15:38:30 +0200

    t

--- venv/dealer.py
+++ venv/dealer.py
@@ -1,8 +1,10 @@
 from deck import Deck
+from player import Player
 
-class Dealer(Deck):
+class Dealer(Player):
     def __init__(self, name = "Dealer"):
         Deck.__init__(self)
+        Player.__init__(self)
         self.name = name
 
     # METHODS
@@ -10,10 +12,17 @@ class Dealer(Deck):
         print(f"The dealer won {win_amt}")
 
     def lose(self, lose_amt):
-        print(f"the dealer lose {lose_amt}")
+        print(f"The dealer lose {lose_amt}")
 
-    def getCard(self):
-        return self.deal()
+    def total_cards(self, *args):
+        total = 0
+        i = 0
+        for card_number in args[i]:
+            total = total + card_number[0]
+            i = i + 1
+        return total
 
+"""
 dealer = Dealer()
 dealer.getCard()
+"""
--- venv/player.py
+++ venv/player.py
@@ -1,7 +1,7 @@
 from deck import Deck
 
 class Player(Deck):
-    def __init__(self, name, balance = 0):
+    def __init__(self, name = "Noone", balance = 100):
         Deck.__init__(self)
         self.name = name
         self.balance = balance
@@ -22,13 +22,21 @@ class Player(Deck):
 
     def choice(self):
         while True:
-                choice = input(f"{self.name}, what do want to do. One more card or not ? (Hit/Stay)").lower()
+                choice = input(f"{self.name}, what do want to do ? One more card or not ? (Hit/Stay)").lower()
                 if choice == 'hit' or choice == 'stay':
                     break
                 else:
                     print(f"{self.name}, please enter the words 'Hit' or 'Stay'")
                     continue
         return choice
+
+    def total_cards(self, *args):
+        total = 0
+        i = 0
+        for card_number in args[i]:
+            total = total + card_number[0]
+            i = i + 1
+        return total
 
     def win(self, win_amt):
         self.balance = self.balance + win_amt
@@ -38,6 +46,16 @@ class Player(Deck):
         self.balance = self.balance - lose_amt
         print(f"You lose {lose_amt}")
 
+    def lost_21(self, total):
+        return f"Your total is {total}. It is over 21. The bank wins."
+
+    def win_21(self):
+        return "21 !! You win."
+
+    def less_21(self, total):
+        return f"Your total is {total}"
+
+"""
 d = Deck()
 player1 = Player("Anthony", 100)
 d.shuffle_cards()
@@ -48,6 +66,7 @@ dealer_fc = (d.deal())
 dealer_sc = (d.deal())
 print(player_fc, player_sc)
 print(dealer_fc, dealer_sc)
+player1.total_cards(player_fc, player_sc)
 pb = player1.bet()
 
 
@@ -55,5 +74,4 @@ if player_fc[0] > player_sc[0]:
     print(player_fc[0], player_fc[1])
 else:
     print(player_sc[0], player_sc[1])
-
-
+"""
